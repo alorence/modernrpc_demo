38460664abf95b775040445472c613df1008feb2
Antoine Lorence <alorence@flux.to>
2017-02-17 08:08:45 +0100

    Add doc (& update requirements)

--- main_app/rpc_methods.py
+++ main_app/rpc_methods.py
@@ -31,6 +31,13 @@ def divide(dividend, divisor):
 
 @rpc_method(name="util.printContentType")
 def content_type_printer(**kwargs):
+    """
+    Inspect request to extract the Content-Type heaser if present.
+    This method demonstrate how a RPC method can access the request object.
+
+    :param kwargs: Dict with current request, protocol and entry_point information.
+    :return: The Content-Type string for incoming request
+    """
     # The other available variables are:
     # protocol = kwargs.get(MODERNRPC_PROTOCOL_PARAM_NAME)
     # entry_point = kwargs.get(MODERNRPC_ENTRY_POINT_PARAM_NAME)
