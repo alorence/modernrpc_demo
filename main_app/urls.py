a791f473ba78e4822f38a12ab205f56de8ac4d2d
Antoine Lorence <antoine.lorence@gmail.com>
2017-05-12 14:11:31 +0200

    Fix warning related to django 2.0 deprecation

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,8 @@ from modernrpc.views import RPCEntryPoint
 
 from main_app.views import HomePageView
 
+app_name = 'main'
+
 urlpatterns = [
     url(r'^$', HomePageView.as_view()),
     url(r'^rpc', RPCEntryPoint.as_view(enable_doc=True)),
