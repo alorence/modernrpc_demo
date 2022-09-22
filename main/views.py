aa940aa337393e8d0fce919bc452795b356341c0
Antoine Lorence <contact@alorence.me>
2022-09-22 15:17:15 +0200

    Update django-modern-rpc to 1.0.0a3

--- main/views.py
+++ main/views.py
@@ -9,7 +9,7 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         ctx = super(HomePageView, self).get_context_data(**kwargs)
         ctx.update({
-            'modernrpc_version': modernrpc.__version__,
+            'modernrpc_version': pkg_resources.get_distribution("django-modern-rpc").version,
             'methods': registry.get_all_methods(sort_methods=True),
         })
         return ctx
