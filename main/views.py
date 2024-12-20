14ce9b0be2821d763d4975a053f3a8e58010ddaf
Antoine Lorence <alorence@flokod.com>
2024-12-20 09:41:11 +0100

    Replaces requirements.txt with poetry.lock

--- main/views.py
+++ main/views.py
@@ -1,15 +1,17 @@
-import pkg_resources
+import importlib.metadata
+
 from django.views.generic.base import TemplateView
+
 from modernrpc.core import registry
 
 
 class HomePageView(TemplateView):
-    template_name = 'main/home.html'
+    template_name = "main/home.html"
 
     def get_context_data(self, **kwargs):
         ctx = super(HomePageView, self).get_context_data(**kwargs)
         ctx.update({
-            'modernrpc_version': pkg_resources.get_distribution("django-modern-rpc").version,
-            'methods': registry.get_all_methods(sort_methods=True),
+            "modernrpc_version": importlib.metadata.metadata("django-modern-rpc")["Version"],
+            "methods": registry.get_all_methods(sort_methods=True),
         })
         return ctx
