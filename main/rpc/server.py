8022a57a7516fa9650f168e52a63a3962572b267
Antoine Lorence <alorence@flokod.com>
2025-07-05 20:58:42 +0200

    Configure Sentry error handling

--- main/rpc/server.py
+++ main/rpc/server.py
@@ -1,10 +1,17 @@
-from modernrpc import RpcServer
+import sentry_sdk
+
+from modernrpc import RpcRequestContext, RpcServer
 
 from .errors import errors
 from .math import math
 from .meta import meta
 
-server = RpcServer()
+
+def main_error_handler(exc: BaseException, _: RpcRequestContext) -> None:
+    sentry_sdk.capture_exception(exc)
+
+
+server = RpcServer(error_handler=main_error_handler)
 
 server.register_namespace(errors)
 server.register_namespace(math)
--- modernrpc_demo/settings.py
+++ modernrpc_demo/settings.py
@@ -11,89 +11,90 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import environ
+import sentry_sdk
 
 # Configure the project's root
-root = environ.Path(__file__, '../..')
+root = environ.Path(__file__, "../..")
 
 # Configure some default for environment
 env = environ.Env(DEBUG=(bool, False),)
 # Try to load
-environ.Env.read_env(root('.env'))
+environ.Env.read_env(root(".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY', default='dummy_local_secret_key_xyz')
+SECRET_KEY = env("SECRET_KEY", default="dummy_local_secret_key_xyz")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = [
-    'modernrpc.onrender.com',
-    'localhost', '127.0.0.1', '[::1]',
+    "modernrpc.onrender.com",
+    "localhost", "127.0.0.1", "[::1]",
 ]
 
 INTERNAL_IPS = [
-    '127.0.0.1',
-    'localhost',
+    "127.0.0.1",
+    "localhost",
 ]
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 
-    'modernrpc',
-    'analytical',
+    "modernrpc",
+    "analytical",
 
-    'main',
+    "main",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+    "django.middleware.security.SecurityMiddleware",
 
     # Immediately after SecurityMiddleware
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'modernrpc_demo.urls'
+ROOT_URLCONF = "modernrpc_demo.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'modernrpc_demo.wsgi.application'
+WSGI_APPLICATION = "modernrpc_demo.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': env.db('DATABASE_URL', 'sqlite:///db.sqlite3')
+    "default": env.db("DATABASE_URL", "sqlite:///db.sqlite3")
 }
 
 
@@ -102,36 +103,36 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
 ENABLE_DEBUG_LOGS = env.bool("ENABLE_DEBUG_LOGS", default=False)
 if ENABLE_DEBUG_LOGS:
     LOGGING = {
-        'version': 1,
-        'disable_existing_loggers': False,
-        'handlers': {
-            'console': {
-                'level': 'DEBUG',
-                'class': 'logging.StreamHandler',
+        "version": 1,
+        "disable_existing_loggers": False,
+        "handlers": {
+            "console": {
+                "level": "DEBUG",
+                "class": "logging.StreamHandler",
             },
         },
-        'loggers': {
+        "loggers": {
             # your other loggers configuration
-            'modernrpc': {
-                'handlers': ['console'],
-                'level': 'DEBUG',
-                'propagate': True,
+            "modernrpc": {
+                "handlers": ["console"],
+                "level": "DEBUG",
+                "propagate": True,
             },
         }
     }
@@ -139,8 +140,8 @@ if ENABLE_DEBUG_LOGS:
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
@@ -149,11 +150,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = root('static')
+STATIC_URL = "/static/"
+STATIC_ROOT = root("static")
 
 # Whitenoise static file compression
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Force HTTPS on production:
 SECURE_SSL_REDIRECT = False  # not DEBUG
@@ -161,4 +162,17 @@ SECURE_SSL_REDIRECT = False  # not DEBUG
 # Django-analytical
 GOOGLE_ANALYTICS_SITE_SPEED = True
 GOOGLE_ANALYTICS_ANONYMIZE_IP = True
-GOOGLE_ANALYTICS_GTAG_PROPERTY_ID = env.str('GTAG_PROPERTY_ID', default="")
+GOOGLE_ANALYTICS_GTAG_PROPERTY_ID = env.str("GTAG_PROPERTY_ID", default="")
+
+
+# Sentry
+if SENTRY_DSN := env("SENTRY_DSN", default=""):
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        # Add data like request headers and IP for users,
+        # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+        send_default_pii=True,
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        traces_sample_rate=1.0,
+    )
