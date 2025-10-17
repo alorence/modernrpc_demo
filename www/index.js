2a3630bc88a681ca9b87588e7e2a406cbeb3a3ce
Antoine Lorence <alorence@flokod.com>
2025-10-17 10:33:50 +0200

    [js] Setup Alpine.js

--- www/index.js
+++ www/index.js
@@ -1,7 +1,10 @@
 import "flyonui/dist/tabs"
+import Alpine from 'alpinejs'
 
 import InitApp from "./init.js"
 
 InitApp(function () {
+  window.Alpine = Alpine
+  Alpine.start()
   console.log("JS initialization is done")
 })
