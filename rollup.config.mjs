ba8cff89fc15a28824b0658aa092db11c5acf721
Antoine Lorence <alorence@flokod.com>
2025-10-20 08:41:26 +0200

    Configure quick copy of generated example

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,9 +1,19 @@
 import nodeResolve from "@rollup/plugin-node-resolve";
+import copy from 'rollup-plugin-copy'
 
 export default {
   input: 'www/index.js',
   output: {
     file: 'dist/app.js',
   },
-  plugins: [nodeResolve()]
+  plugins: [
+    nodeResolve(),
+    copy({
+      verbose: true,
+      targets: [
+        { src: 'node_modules/clipboard/dist/clipboard.js', dest: 'dist' },
+        { src: 'node_modules/flyonui/dist/helper-clipboard.js', dest: 'dist' },
+      ]
+    })
+  ]
 }
