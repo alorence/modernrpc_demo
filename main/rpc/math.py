80812795510f7d9f78d33e40a93b5c68adf7c364
Antoine Lorence <alorence@flokod.com>
2025-10-23 18:31:41 +0200

    Rework procedures docstrings

--- main/rpc/math.py
+++ main/rpc/math.py
@@ -6,12 +6,13 @@ math = RpcNamespace()
 @math.register_procedure(name="math.add")
 def add(term_a: int | float, term_b: int | float) -> int | float:
     """
-    Add termA and termB, end return the result
+    Returns the sum of two terms.
 
+    This procedure can be used to test different types of inputs. a and b terms can be passed as int or float, the
+    result type will depends on the input ones.
     :param term_a: First term
     :param term_b: Second term
     :return: Sum of the two terms
-    :rtype: int, float
     """
     return term_a + term_b
 
@@ -19,10 +20,12 @@ def add(term_a: int | float, term_b: int | float) -> int | float:
 @math.register_procedure(name="math.divide")
 def divide(dividend: int | float, divisor: int | float):
     """
-    Divide the dividend by the divisor
+    Returns the division result of two numbers.
 
-    :param dividend: Number
-    :param divisor: Number
-    :return: Result of the division
+    This procedure can be used to raise an exception. If divisor is set to 0, a ZeroDivisionError will be raised from
+    the function and transformed into a proper RPC Error response.
+    :param dividend: First term of the division
+    :param divisor: Second term of the division
+    :return: The result
     """
     return dividend / divisor
