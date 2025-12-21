5582151e18409ed58af82f13ffd1ca7c157ea861
Antoine Lorence <alorence@flokod.com>
2025-12-21 05:21:37 +0100

    Update all

--- www/init.js
+++ www/init.js
@@ -2,11 +2,11 @@ const InitApp = fn => {
   console.log(`[InitApp] document.readyState === ${document.readyState}`)
   if (document.readyState === "loading") {
     console.log('[InitApp] ⏳ Schedule initialization on "DOMContentLoaded" event')
-    document.addEventListener("DOMContentLoaded", fn);
+    document.addEventListener("DOMContentLoaded", fn)
   } else {
     console.log(`[InitApp] ➞ Call initialization function synchronously`)
-    fn();
+    fn()
   }
 }
 
-export default InitApp
+export default InitApp;
